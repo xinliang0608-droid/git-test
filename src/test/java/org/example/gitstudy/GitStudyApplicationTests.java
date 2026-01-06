a883554590f121491036b6cc74d2ad83088f6cbf
xinliang <981050926@qq.com>
2026-01-06 15:30:09 +0800

    java first commit

---
+++ src/test/java/org/example/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
